376d5bb5a53467ab391a2707f3398d77321121b2
Vitor Gomes Pereira <41605815+vitor7001@users.noreply.github.com>
2023-12-08 23:14:02 -0300

    test: criando Category com isActive

--- tests/Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -30,7 +30,38 @@ public class CategoryTest
         Assert.NotEqual(default(DateTime), category.CreatedAt);
         Assert.True(category.CreatedAt > dateTimeBefore);
         Assert.True(category.CreatedAt < dateTimeAfter);
-        Assert.True(category.isActive);
+        Assert.True(category.IsActive);
+    }
+
+    [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        //Arrange
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description"
+        };
+
+        var dateTimeBefore = DateTime.Now;
+
+        //Act
+        var category = new DomainEntity.Category(validData.Name, validData.Description, isActive);
+
+        var dateTimeAfter = DateTime.Now;
+
+        //Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt > dateTimeBefore);
+        Assert.True(category.CreatedAt < dateTimeAfter);
+        Assert.Equal(isActive , category.IsActive);
     }
 }
 
