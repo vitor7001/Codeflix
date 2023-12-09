b8c312447cd12130b16fe98d379a1e6b9e48267b
Vitor Gomes Pereira <41605815+vitor7001@users.noreply.github.com>
2023-12-08 23:14:25 -0300

    feat: passando valores para construtor

--- src/Codeflix.Catalog.Domain/Entity/Category.cs
+++ src/Codeflix.Catalog.Domain/Entity/Category.cs
@@ -4,15 +4,15 @@ public class Category
     public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
-    public bool isActive { get; private set; }
+    public bool IsActive { get; private set; }
     public DateTime CreatedAt { get; private set; }
 
-    public Category(string name, string description)
+    public Category(string name, string description, bool isActive = true)
     {
         Id = Guid.NewGuid();
         Name = name;
         Description = description;
-        isActive = true;
+        IsActive = isActive;
         CreatedAt = DateTime.Now;
     }
 }
