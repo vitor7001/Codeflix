63eb61c381e8ed827d5e1179d2e07a4e1474468f
Vitor Gomes Pereira <41605815+vitor7001@users.noreply.github.com>
2023-12-18 22:13:10 -0300

    refactor: separando validação para domainValidation

---
+++ src/Codeflix.Catalog.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,14 @@
+﻿using Codeflix.Catalog.Domain.Exceptions;
+
+namespace Codeflix.Catalog.Domain.Validation;
+public class DomainValidation
+{
+    public static void NotNull(object target, string fieldName)
+    {
+        if (target is null)
+        {
+            throw new EntityValidationException($"{fieldName} should not be null");
+        }
+
+    }
+}
---
+++ tests/Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,44 @@
+﻿using Bogus;
+using Xunit;
+
+using Codeflix.Catalog.Domain.Validation;
+using FluentAssertions;
+using Codeflix.Catalog.Domain.Exceptions;
+
+namespace Codeflix.Catalog.UnitTests.Domain.Validation;
+public class DomainValidationTest
+{
+    private Faker Faker { get; set; } = new Faker();
+
+    [Fact(DisplayName = nameof(ShouldHaveSucessWithNotNullValue))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void ShouldHaveSucessWithNotNullValue()
+    {
+        var value = Faker.Commerce.ProductName();
+
+        Action action =
+            () => DomainValidation.NotNull(value, "value");
+
+        action.Should().NotThrow();
+    }
+
+    [Fact(DisplayName = nameof(ShouldHaveErrorWithNullValue))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void ShouldHaveErrorWithNullValue()
+    {
+        string value = null;
+
+        Action action =
+            () => DomainValidation.NotNull(value, "FieldName");
+
+        action.Should().Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null");
+    }
+
+    // nao ser null ou vazio
+
+    // tamanho minimo
+    // tamanho maximo
+
+
+}
